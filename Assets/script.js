495f39ad2ade83765226ad8cda84401f02495561
Yasmin Bustami <yasminbustami@Yasmins-MacBook-Pro.local>
2022-09-13 22:42:40 -0500

    js editing

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+// variables being set for city etc
+
+
+let search = document.querySelector("#search");
+let temp = document.querySelector("#temp");
+let wind = document.querySelector("#wind");
+let humidity = document.querySelector("#humidity");
+let uvindex = document.querySelector("#uv-index");
+let currentlocation = document.querySelector("#Current-Location");
+let currenticon = document.querySelector("#current-icon");
