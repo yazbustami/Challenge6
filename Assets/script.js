4a592d5319a11778fda44e3d669445260954fbf4
Yasmin Bustami <yasminbustami@Yasmins-MacBook-Pro.local>
2022-09-13 22:47:24 -0500

    js forecast day variables

--- Assets/script.js
+++ Assets/script.js
@@ -8,3 +8,8 @@ let humidity = document.querySelector("#humidity");
 let uvindex = document.querySelector("#uv-index");
 let currentlocation = document.querySelector("#Current-Location");
 let currenticon = document.querySelector("#current-icon");
+
+let city;
+let cityArray = [];
+let today = moment().format('MM/DD/YYYY');
+// forecast days
